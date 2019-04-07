fdf41f19bd3a7e50c360098035a27537839d77eb
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2019-04-07 17:39:20 +0200

    yaml support

--- internal/azure/rules/types.go
+++ internal/azure/rules/types.go
@@ -1,8 +1,12 @@
 package rules
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"unicode"
+
+	"github.com/ghodss/yaml"
 
 	"github.com/pkg/errors"
 )
@@ -52,9 +56,35 @@ func (p ActionItem) GetType() string {
 func parseRulesDefinitions(rules string) (TagRules, error) {
 	var rulesDef TagRules
 	byt := []byte(rules)
-	if err := json.Unmarshal(byt, &rulesDef); err != nil {
-		return TagRules{}, errors.Wrap(err, "can't unmarshal rules")
+	if hasJSONPrefix(byt) {
+		if err := json.Unmarshal(byt, &rulesDef); err != nil {
+			return TagRules{}, errors.Wrap(err, "can't unmarshal json rules")
+		}
+	} else {
+		if err := yaml.Unmarshal(byt, &rulesDef); err != nil {
+			return TagRules{}, errors.Wrap(err, "can't unmarshal yaml rules")
+		}
 	}
-
 	return rulesDef, nil
+}
+
+var jsonPrefix = []byte("{")
+
+// hasJSONPrefix returns true if the provided buffer appears to start with
+// a JSON open brace.
+func hasJSONPrefix(buf []byte) bool {
+	return hasPrefix(buf, jsonPrefix)
+}
+
+// Return true if the first non-whitespace bytes in buf is prefix.
+func hasPrefix(buf []byte, prefix []byte) bool {
+	trim := bytes.TrimLeftFunc(buf, unicode.IsSpace)
+	return bytes.HasPrefix(trim, prefix)
+}
+
+func ToJSON(data []byte) ([]byte, error) {
+	if hasJSONPrefix(data) {
+		return data, nil
+	}
+	return yaml.YAMLToJSON(data)
 }
